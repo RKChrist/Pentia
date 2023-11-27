f7ff9b3a45e5431f4c9c46f8290b05a0dbcc8786
RKChrist <roo12ney@gmail.com>
2023-11-27 18:44:11 +0100

    Update to splash_screen_steps

--- features/step-definitions/splash_screen_steps.js
+++ features/step-definitions/splash_screen_steps.js
@@ -1,25 +1,25 @@
 const { Given, Then } = require('@cucumber/cucumber');
 
-Given('the application is launched', function () {
-  // Mock the application launch process
-});
-
-Then('the splash screen should be visible while the application is loading', function () {
-  // Verify the visibility of the splash screen during loading
-});
-
 Given('the application has finished loading', function () {
   // Mock the completion of the application loading process
 });
 
+Given('I am logged in', function () {
+  // Mock a state where the user is considered logged in
+});
+
 Then('the next screen should fade in', function () {
-  // Verify that the next screen fades in after loading
+  // Assert or verify that the next screen transition includes a fade-in effect
 });
 
-Then('it should go to Chat rooms if I am logged in', function () {
-  // Verify navigation to Chat rooms for logged-in users
+Then('it should go to Chat rooms', function () {
+  // Assert or verify that the user is navigated to the Chat rooms screen
 });
 
-Then('it should go to the Login screen if I am not logged in', function () {
-  // Verify navigation to the Login screen for not logged-in users
-});
+Given('I am not logged in', function () {
+    // Mock a state where the user is considered not logged in
+  });
+  
+  Then('it should go to the Login screen', function () {
+    // Assert or verify that the user is navigated to the Login screen
+  });
